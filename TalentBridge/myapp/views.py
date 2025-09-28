ac7d7738eb8a7b7f5fe6ba50019ff1541548e37e
Isoo Pal <17isoo12@gmail.com>
2025-09-28 16:48:32 +0530

    Added some css in 'How It Works' section

--- TalentBridge/myapp/views.py
+++ TalentBridge/myapp/views.py
@@ -3,4 +3,4 @@ from django.conf import settings
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "myapp/home.html")
+    return render(request, "myapp/index.html")
