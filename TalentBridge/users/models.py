bb1529f8bed4f5872534c49ed7b3f65fb8cf6c10
Isoo Pal <17isoo12@gmail.com>
2025-10-24 16:45:42 +0530

    User Registration form

---
+++ TalentBridge/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    country = CountryField(blank_label = 'Select Country')
+    state = models.CharField(max_length=100, blank=true)
+    city = models.CharField(max_length=100)
+    zip = models.IntegerField()
+    userType = models.CharField(max_length=100)
+    skills = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
