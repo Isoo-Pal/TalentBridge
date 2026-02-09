a56d42a06df163c950daaf0e876c37164b8ca015
Isoo Pal <17isoo12@gmail.com>
2026-02-09 11:47:58 +0530

    Worked on registration form submission

--- TalentBridge/users/views.py
+++ TalentBridge/users/views.py
@@ -3,6 +3,18 @@ from .forms import *
 
 # Create your views here.
 def registration(request):
-    user_form = UserRegisterForm()
-    profile_form = UserProfileForm()
+    if request.method == 'POST':
+        user_form = UserRegisterForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            print("Form saved")
+    else:
+        user_form = UserRegisterForm()
+        profile_form = UserProfileForm()
     return render(request, "users/registration.html", {'profile_form' : profile_form, 'user_form': user_form})
