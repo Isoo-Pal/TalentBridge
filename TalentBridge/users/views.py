d8fd450b34a7456c8fcfaa26bdf7a60036f7addd
Isoo Pal <17isoo12@gmail.com>
2026-02-12 11:27:40 +0530

    Used messages framework for showing tags

--- TalentBridge/users/views.py
+++ TalentBridge/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 def registration(request):
@@ -7,20 +8,19 @@ def registration(request):
         print(request.POST)
         user_form = UserRegisterForm(request.POST)
         profile_form = UserProfileForm(request.POST)
-        
-        print(user_form.is_valid(), profile_form.is_valid())
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
 
             profile = profile_form.save(commit=False)
             profile.user = user
             profile.save()
-
-            print("Form saved")
+            messages.success(request, "Registration successful!!!")
+        else:
+            messages.error(request, "Form Invalid!!!")
     else:
         user_form = UserRegisterForm()
         profile_form = UserProfileForm()
-
-    print(user_form.errors, profile_form.errors)
+    # messages.warning(request, "Registration successful!!!")
+    # print(user_form.errors, profile_form.errors)
         
     return render(request, "users/registration.html", {'profile_form' : profile_form, 'user_form': user_form})
