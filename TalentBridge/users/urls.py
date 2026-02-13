a9edd3ba05dcafee2a65498d87b04f57335220cb
Isoo Pal <17isoo12@gmail.com>
2026-02-13 10:59:07 +0530

    Using Django's default login authentication

--- TalentBridge/users/urls.py
+++ TalentBridge/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', views.registration, name="registration"),
+    path('login/', auth_views.LoginView.as_view(), name = "login"),
 ]
--- TalentBridge/users/views.py
+++ TalentBridge/users/views.py
@@ -1,26 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib import messages
 
 # Create your views here.
-def registration(request):
-    if request.method == 'POST':
-        print(request.POST)
-        user_form = UserRegisterForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
 
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            messages.success(request, "Registration successful!!!")
-        else:
-            messages.error(request, "Form Invalid!!!")
-    else:
-        user_form = UserRegisterForm()
-        profile_form = UserProfileForm()
+
+def registration(request):
+    # if request.method == 'POST':
+    #     print(request.POST)
+    #     user_form = UserRegisterForm(request.POST)
+    #     profile_form = UserProfileForm(request.POST)
+    #     if user_form.is_valid() and profile_form.is_valid():
+    #         user = user_form.save()
+
+    #         profile = profile_form.save(commit=False)
+    #         profile.user = user
+    #         profile.save()
+    #         messages.success(request, "Registration successful!!!")
+    #         return redirect('login')
+    #     else:
+    #         messages.error(request, "Form Invalid!!!")
+    # else:
+    #     user_form = UserRegisterForm()
+    #     profile_form = UserProfileForm()
     # messages.warning(request, "Registration successful!!!")
     # print(user_form.errors, profile_form.errors)
         
-    return render(request, "users/registration.html", {'profile_form' : profile_form, 'user_form': user_form})
+    # return render(request, "users/registration.html", {'profile_form' : profile_form, 'user_form': user_form})
+    return redirect("users:login")
