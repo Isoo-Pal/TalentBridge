c15105d413a10317ee7ed67ca71d36bb928b3084
Isoo Pal <17isoo12@gmail.com>
2026-02-19 11:03:02 +0530

    Designed Login form

--- TalentBridge/users/views.py
+++ TalentBridge/users/views.py
@@ -6,25 +6,27 @@ from django.contrib import messages
 
 
 def registration(request):
-    # if request.method == 'POST':
-    #     print(request.POST)
-    #     user_form = UserRegisterForm(request.POST)
-    #     profile_form = UserProfileForm(request.POST)
-    #     if user_form.is_valid() and profile_form.is_valid():
-    #         user = user_form.save()
+    if request.user.is_authenticated:
+        return redirect('users:login')
+    if request.method == 'POST':
+        print(request.POST)
+        user_form = UserRegisterForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
 
-    #         profile = profile_form.save(commit=False)
-    #         profile.user = user
-    #         profile.save()
-    #         messages.success(request, "Registration successful!!!")
-    #         return redirect('login')
-    #     else:
-    #         messages.error(request, "Form Invalid!!!")
-    # else:
-    #     user_form = UserRegisterForm()
-    #     profile_form = UserProfileForm()
-    # messages.warning(request, "Registration successful!!!")
-    # print(user_form.errors, profile_form.errors)
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            messages.success(request, "Registration successful!!!")
+            return redirect('users:login')
+        else:
+            messages.error(request, "Form Invalid!!!")
+    else:
+        user_form = UserRegisterForm()
+        profile_form = UserProfileForm()
+    messages.warning(request, "Registration successful!!!")
+    print(user_form.errors, profile_form.errors)
         
-    # return render(request, "users/registration.html", {'profile_form' : profile_form, 'user_form': user_form})
-    return redirect("users:login")
+    return render(request, "users/registration.html", {'profile_form' : profile_form, 'user_form': user_form})
+    # return redirect("users:login")
